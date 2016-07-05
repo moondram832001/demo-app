67328b27ebbed3401331572e20f46ba678b6560d
moondram832001 <moondram832001@gmail.com>
2016-07-05 13:17:51 +0530

    built files, file cleanup

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
   app.use(express.static(__dirname + '/dist'));
   
   var routes = require('./apis')(app);
@@ -18,10 +12,6 @@ app.set('port', (process.env.PORT || 5000));
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
 
-
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
